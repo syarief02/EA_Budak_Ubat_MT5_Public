0a2722a4ea9dba0323606dacb020cf94e544ebdb
Syarief Azman <syarief.azman@gmail.com>
2026-08-29 02:52:33 +0800

    feat(brokers): add FISG to centralized partner broker list

--- ea-budak-ubat-web/lib/authorizedAccounts.js
+++ ea-budak-ubat-web/lib/authorizedAccounts.js
@@ -2,6 +2,7 @@
 // Source: MQL4 & MQL5 expert advisors
 
 export const BROKERS = [
+  { name: "FISG", id: "CTt0Rd", url: "https://my.fisg.com/u/CTt0Rd", support: "support@fisg.com" },
   { name: "CXM Direct", id: "5062", url: "https://gocxm.co/links/go/5062", support: "clients@cxmdirect.com" },
   { name: "FBS", id: "588292", url: "https://fbs.partners?ibl=154319&ibp=588292", support: "support@fbs.com" },
   { name: "Markets4you", id: "4hcnvz4", url: "https://account.markets4you.online/en/user-registration/?affid=4hcnvz4", support: "support@markets4you.com" },
