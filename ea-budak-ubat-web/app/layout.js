10f8772bb040829e8692e170263a0c4f22508502
Syarief Azman <syarief.azman@gmail.com>
2026-09-08 20:35:13 +0800

    chore: format Meta Pixel script in head

--- ea-budak-ubat-web/app/layout.js
+++ ea-budak-ubat-web/app/layout.js
@@ -20,12 +20,22 @@ export default function RootLayout({ children }) {
     <html lang="en">
       <head>
         <script
+          id="fb-pixel"
           dangerouslySetInnerHTML={{
-            __html: `!function(f,b,e,v,n,t,s){if(f.fbq)return;n=f.fbq=function(){n.callMethod?n.callMethod.apply(n,arguments):n.queue.push(arguments)};if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';n.queue=[];t=b.createElement(e);t.async=!0;t.src=v;s=b.getElementsByTagName(e)[0];s.parentNode.insertBefore(t,s)}(window, document,'script','https://connect.facebook.net/en_US/fbevents.js');fbq('init', '2242456612962821');fbq('track', 'PageView');`,
+            __html: `
+              !function(f,b,e,v,n,t,s)
+              {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+              n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+              if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+              n.queue=[];t=b.createElement(e);t.async=!0;
+              t.src=v;s=b.getElementsByTagName(e)[0];
+              s.parentNode.insertBefore(t,s)}(window, document,'script',
+              'https://connect.facebook.net/en_US/fbevents.js');
+              fbq('init', '2242456612962821');
+              fbq('track', 'PageView');
+            `,
           }}
         />
-      </head>
-      <body>
         <noscript>
           <img
             height="1"
@@ -35,6 +45,8 @@ export default function RootLayout({ children }) {
             alt=""
           />
         </noscript>
+      </head>
+      <body>
         <Suspense fallback={null}>
           <FacebookPixel />
         </Suspense>
--- ea-budak-ubat-web/lib/fpixel.js
+++ ea-budak-ubat-web/lib/fpixel.js
@@ -20,3 +20,4 @@ export const customEvent = (name, options = {}) => {
     window.fbq("trackCustom", name, options);
   }
 };
+
